241545ef5bbf98138beb1c91722853434df4e5a5
IrvingYHM <hernandezirving725@gmail.com>
2024-09-18 10:02:07 -0600

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="flex flex-col min-h-screen">
+        <main className="flex-grow p-8 text-center">
+          <h1 className="text-4xl font-bold mb-4">Bienvenido a Mi Sitio Web</h1>
+          <p>Este es un sitio web simple creado con React.</p>
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default App
