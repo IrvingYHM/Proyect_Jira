0123aa16d37e390dfd63477e17e61b4b8e0bb975
Julio <julio.ce01@hotmail.com>
2024-09-18 13:23:23 -0600

    PJ-1 <Creacion del header>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 const Header = () => {
   return (
     <header className="bg-blue-600 p-4 text-white flex justify-between items-center">
-      <h1 className="text-2xl font-bold">Mi Sitio Web</h1>
+      <h1 className="text-2xl font-bold">Mi Sitio</h1>
       <nav>
         <ul className="flex space-x-4">
           <li>
             <a href="#home" className="hover:underline">Inicio</a>
           </li>
           <li>
-            <a href="#about" className="hover:underline">Acerca de</a>
+            <a href="#about" className="hover:underline">Acerca de </a>
           </li>
           <li>
             <a href="#services" className="hover:underline">Servicios</a>
