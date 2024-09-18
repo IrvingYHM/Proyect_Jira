208e6908c76d9691d2ca6f18d919d90a40c604a2
Julio <julio.ce01@hotmail.com>
2024-09-18 13:19:10 -0600

    creacion del Header

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +11,7 @@ function App() {
   return (
     <>
       <div className="flex flex-col min-h-screen">
+        <Header/>
         <main className="flex-grow p-8 text-center">
           <h1 className="text-4xl font-bold mb-4">Bienvenido a Mi Sitio Web</h1>
           <p>Este es un sitio web simple creado con React.</p>
